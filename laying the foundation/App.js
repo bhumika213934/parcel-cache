4b20c5ae15e1fd34ea622339770eac41b14db144
bhumika <bhumikamishra1122@gmail.com>
2026-04-25 10:06:19 +0530

    jsx

--- laying the foundation/App.js
+++ laying the foundation/App.js
@@ -1,13 +1,37 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-    const list= React.createElement("ul",{
+//     const list= React.createElement("ul",{
 
-    },[React.createElement("li",{key:"lo1",className:"BHUMIKA"},"hemloo"),
-        React.createElement("li",{key:"lo2",className:"BHUMIKA"},"helloooooooooooooooooooooooooooooooooooo"),
-        React.createElement("li",{key:"lo3",className:"BHUMIKA"},"heyiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii")
-    ]
-);
-console.log(list);
+//     },[React.createElement("li",{
+//         className:"BHUMIKA"},"hemloo"),
+//         React.createElement("li",{key:"lo2",className:"BHUMIKA"},"helloooooooooooooooooooooooooooooooooooo"),
+//         React.createElement("li",{key:"lo3",className:"BHUMIKA"},"heyiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii")
+//     ]
+// );
+// console.log(list);
 
-const root = ReactDOM.createRoot(document.getElementById("root"))
-root.render(list);
+// const root = ReactDOM.createRoot(document.getElementById("root"))
+// root.render(list);
+// const heading = <h1>hello world</h1>;
+// const heading2 = (
+//   <div id="hey" className="bhumi" key="h1">
+//     hola
+//   </div>
+// );
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(heading);
+
+const Headercomponent = () => {
+  return (
+    <div>
+      <h1>logo</h1>
+      <Logo />
+    </div>
+  );
+};
+const Logo = () => {
+  return <h1>Bhumika</h1>;
+};
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Headercomponent />);
