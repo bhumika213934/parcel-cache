b9627266225bf4b783851b973e12b313abad0753
bhumika <bhumikamishra1122@gmail.com>
2026-05-22 10:02:41 +0530

    props

--- talkycheatshowmethecode/components/ResturantCard.js
+++ talkycheatshowmethecode/components/ResturantCard.js
@@ -1,26 +1,16 @@
-const ResturantCard = () => {
-  const resturantdata = [
-    {
-      Name: "Domino",
-      image:
-        "https://cdn.britannica.com/08/177308-050-94D9D6BE/Food-Pizza-Basil-Tomato.jpg",
-      price: "350 for 2",
-      cuisine: ["north Indian,south Indian"],
-    },
-    {
-      Name: "Domino",
-      image:
-        "https://cdn.britannica.com/08/177308-050-94D9D6BE/Food-Pizza-Basil-Tomato.jpg",
-      price: "350 for 2",
-      cuisine: ["north Indian,south Indian"],
-    },
-  ];
+import { imageUrl } from "../CONSTANT.JS";
+const ResturantCard = (props) => {
+  const { name, cuisines, costForTwo, cloudinaryImageId } = props.data.info;
+  console.log(props.data.info);
+  console.log(props);
+  console.log(typeof props);
+
   return (
     <div className="card">
-      <img src={resturantdata[0].image} alt={resturantdata.Name} />
-      <h2>{resturantdata[0].Name}</h2>
-      <div>{resturantdata[0].cuisine}</div>
-      <h3>{resturantdata[0].price}</h3>
+      <img src={imageUrl + cloudinaryImageId} alt="" />
+      <h1>{name}</h1>
+      <div>{cuisines.join(", ")}</div>
+      <h2>{costForTwo}</h2>
     </div>
   );
 };
--- talkycheatshowmethecode/components/ResturantList.js
+++ talkycheatshowmethecode/components/ResturantList.js
@@ -1,16 +1,20 @@
 import ResturantCard from "./ResturantCard";
+import { restaurantData } from "../CONSTANT.JS";
 const ResturantList = () => {
   return (
     <div className="resturantlist">
-      <ResturantCard />
-      <ResturantCard />
-      <ResturantCard />
-      <ResturantCard />
-      <ResturantCard />
-      <ResturantCard />
-      <ResturantCard />
-      <ResturantCard />
-      <ResturantCard />
+      <h1 className="text-9xl">restaurantlist</h1>
+      {restaurantData.map((el, index) => {
+        console.log(el);
+        return <ResturantCard data={el} key={el.info.id} />;
+      })}
+      {/* <ResturantCard data={restaurantData[0]} />
+      <ResturantCard data={restaurantData[1]} />
+      <ResturantCard data={restaurantData[2]} />
+      <ResturantCard data={restaurantData[3]} />
+      <ResturantCard data={restaurantData[4]} />
+      <ResturantCard data={restaurantData[5]} />
+      <ResturantCard abc="abc" data={restaurantData[6]} /> */}
     </div>
   );
 };
